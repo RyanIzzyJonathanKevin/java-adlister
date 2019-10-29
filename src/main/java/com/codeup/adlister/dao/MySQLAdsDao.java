fdc699b773ff396a9abb0d3f4487ae9b3176a621
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-10-29 12:04:05 -0500

    added click on ad and show ad functionality

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -18,7 +18,7 @@ public class MySQLAdsDao implements Ads {
             DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
                 config.getUrl(),
-                config.getUser(),
+                config.getUsername(),
                 config.getPassword()
             );
         } catch (SQLException e) {
@@ -58,7 +58,7 @@ public class MySQLAdsDao implements Ads {
     private Ad extractAd(ResultSet rs) throws SQLException {
         return new Ad(
             rs.getLong("id"),
-            rs.getLong("user_id"),
+            rs.getLong("users_id"),
             rs.getString("title"),
             rs.getString("description")
         );
@@ -71,4 +71,21 @@ public class MySQLAdsDao implements Ads {
         }
         return ads;
     }
+
+    public Ad findAdById(long id) {
+        PreparedStatement ps = null;
+        try {
+            String insertQuery = "SELECT * FROM ads WHERE id = ?";
+            ps = connection.prepareStatement(insertQuery);
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();
+            if(! rs.next()) {
+                return null;
+            }
+            return extractAd(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving current ad.", e);
+        }
+    }
+
 }
